2890a8c0516b7659101fc4ad49fee0b92c0d33cb
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:42:54 -0400

    Update
    Displaying the result of the calculation to the user

--- Own Expressions/Expressions_Personal/JS/javascript.js
+++ Own Expressions/Expressions_Personal/JS/javascript.js
@@ -7,11 +7,16 @@
  */
 //Enrique Di Bello 9/23/13 Assignment Expressions Personal
 
+
 var salary = parseInt(prompt("Please, enter your current salary here"));
+
 var house = parseInt(prompt("Enter the house monthly payment"));
+
 var other = parseInt(prompt("Enter other monthly payments"));
 
 monthlyG = salary - house - other;
+
+console.log(monthlyG);
 
 
 
