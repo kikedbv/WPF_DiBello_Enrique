5b35eb5276952fbf5e792aa1bca0add13a378add
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:49:42 -0400

    Update
    Finallizing the code

--- Own Expressions/Expressions_Personal/JS/javascript.js
+++ Own Expressions/Expressions_Personal/JS/javascript.js
@@ -13,7 +13,7 @@ var salary = parseInt(prompt("Please, enter your current salary here"));
 var house = parseInt(prompt("Enter the house monthly payment"));
 //Is asking the user to enter other monthly payments he/she have
 var other = parseInt(prompt("Enter other monthly payments"));
-
+//Is making subtracting the house and others monthly payments from the actual salary to get his final money
 monthlyG = salary - house - other;
-
+//Displaying to the user the result of the calculation
 console.log(monthlyG);
