3544d4369f8dc071acdcacb3bb391b273557c118
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 23:15:56 -0400

    Update
    satrting with the calculations

--- Own Expressions/Expressions_Wacky/JS/javascript.js
+++ Own Expressions/Expressions_Wacky/JS/javascript.js
@@ -9,7 +9,11 @@
 
 var timesBathroom = parseInt(prompt("Please enter the aprx. times you go to bathroom a day "));
 var paperSheets = parseInt(prompt("Please enter the aprx. sheets of paper your bathroom paper have"));
-var sheetsUsed = parseInt(prompt("Enter how many times you take paper from the roll after you ..."));
+var sheetsUsed = parseInt(prompt("Enter aprx. how many times you take paper from the roll after you ..."));
+
+var rollSheets = sheetsUsed * 4;
+var rollTimes = paperSheets - rollSheets;
+var finall = timesBathroom
 
 
 
