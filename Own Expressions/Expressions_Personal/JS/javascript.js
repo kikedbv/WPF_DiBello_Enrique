39fd7aa2f5a504a0032130501a6794d7818b5396
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:28:14 -0400

    New Upload
    Initiallizing of the Own expression personal

---
+++ Own Expressions/Expressions_Personal/JS/javascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: enriquedibello
+ * Date: 9/23/13
+ * Time: 10:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Enrique Di Bello 9/23/13 Assignment Expressions Personal
+
+var
