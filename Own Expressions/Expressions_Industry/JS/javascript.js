17f541af4c013c76ae680ff39574734c0d185044
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 21:57:25 -0400

    New Upload
    Initiallizing the own expressions.

---
+++ Own Expressions/Expressions_Industry/JS/javascript.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: enriquedibello
+ * Date: 9/23/13
+ * Time: 9:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Enrique Di Bello 9/19/13 Assignment Expressions Industry
+
+//Is
+var actualPrice = parseInt(prompt("Please enter the actual price of the product"));
+var discount = parseInt(prompt("Enter the discount desired for the product", "Ex.: 25"));
+
+
