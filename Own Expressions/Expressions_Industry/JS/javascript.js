8ea510a954134d9ae1de27f29e40974096bff5c2
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:07:42 -0400

    Update
    comments on the first part

--- Own Expressions/Expressions_Industry/JS/javascript.js
+++ Own Expressions/Expressions_Industry/JS/javascript.js
@@ -7,8 +7,9 @@
  */
 //Enrique Di Bello 9/19/13 Assignment Expressions Industry
 
-//Is
+//Is asking the user to enter the price of the product.
 var actualPrice = parseInt(prompt("Please enter the actual price of the product"));
+//Is asking the user to enter the discount for the product
 var discount = parseInt(prompt("Enter the discount desired for the product", "Ex.: 25"));
 
 var discPercent = discount / 100;
@@ -16,4 +17,4 @@ var newPrice = actualPrice * discPercent;
 var finalPrice = actualPrice - newPrice
 
 
-console.log("The sale price for the product will be $" + finalPrice + "!")
+console.log("The sale price for the product will be $" + finalPrice + "!");
