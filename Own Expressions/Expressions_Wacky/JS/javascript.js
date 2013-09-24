e4e16b009a05ee532e11b91eb3048432ad7cb3e3
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 23:08:53 -0400

    Upload
    Initiallizing own expressions Wacky

---
+++ Own Expressions/Expressions_Wacky/JS/javascript.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: enriquedibello
+ * Date: 9/23/13
+ * Time: 10:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Enrique Di Bello 9/23/13 Assignment Expressions Wacky
+
+var timesBathroom = parseInt();
+var
