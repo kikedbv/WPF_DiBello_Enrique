565fbad5b760075adf56f6efbfccf3c0f1af5be7
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:47:52 -0400

    Update
    Comments on the variables area

--- Own Expressions/Expressions_Personal/JS/javascript.js
+++ Own Expressions/Expressions_Personal/JS/javascript.js
@@ -7,18 +7,13 @@
  */
 //Enrique Di Bello 9/23/13 Assignment Expressions Personal
 
-
+//Is asking the user to enter hes actual salary
 var salary = parseInt(prompt("Please, enter your current salary here"));
-
+//Is asking the user to enter how much he monthly pays for the house
 var house = parseInt(prompt("Enter the house monthly payment"));
-
+//Is asking the user to enter other monthly payments he/she have
 var other = parseInt(prompt("Enter other monthly payments"));
 
 monthlyG = salary - house - other;
 
 console.log(monthlyG);
-
-
-
-
-
