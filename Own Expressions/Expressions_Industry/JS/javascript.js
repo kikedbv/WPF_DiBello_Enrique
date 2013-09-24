9c2e12b5ee11aff71ebff68e35f4aac6951cf95d
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-23 22:26:08 -0400

    Update
    Minimun changes on the code

--- Own Expressions/Expressions_Industry/JS/javascript.js
+++ Own Expressions/Expressions_Industry/JS/javascript.js
@@ -16,7 +16,7 @@ var discPercent = discount / 100;
 //Is making the price entered by the user times the transformation of the desired discount divided by 100 (previous step)
 var newPrice = actualPrice * discPercent;
 //Is getting the new price by substracting the number gave on the previous spet from the price entered by the user.
-var finalPrice = actualPrice - newPrice
+var finalPrice = actualPrice - newPrice;
 
 //Is displaying the result of the calculation in the console.
 console.log("The sale price for the product will be $" + finalPrice + "!");
