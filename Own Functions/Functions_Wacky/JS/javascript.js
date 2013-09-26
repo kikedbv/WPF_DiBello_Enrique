d7a57165ddfe6a35db08af3fc9e7016d14752a70
kikedbv <enriquedibello@Enriques-MacBook-Pro-4.local>
2013-09-25 23:37:00 -0400

    Upload
    Starting with the Wacky Function

---
+++ Own Functions/Functions_Wacky/JS/javascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: enriquedibello
+ * Date: 9/25/13
+ * Time: 11:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Enrique Di Bello  09/25/2013  Wacky function
+
+var firstInput = "";
